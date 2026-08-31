69f35d0b5929ecb67eff145cb2668d6df5983feb
Pablo Alejandro Perez Acosta <alejandro.perez.acosta@hotmail.com>
2026-08-31 00:03:37 -0500

    tracing info

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,14 +1,16 @@
 
 use axum::{extract::Query, http::StatusCode, Json};
-use tracing::error;
+use tracing::{error, info};
 use crate::{algorithms::run_random_dijkstra, engine::{generate_julia, generate_leaf, generate_mandelbrot}, models::{AccessLog, Bounds, FractalParams, FractalPoint, PersonaTable, fetch_access_logs, fetch_persons}};
 
 
 pub async fn ping() -> StatusCode {
+    info!("ping() called.");
     StatusCode::NO_CONTENT
 }
 
 pub async fn get_all_logs() -> Result<Json<Vec<AccessLog>>, (StatusCode, String)> {
+    info!("get_all_logs() called.");
     fetch_access_logs().await.map(Json).map_err(|e| {
         error!("Database error: {:?}", e);
         (StatusCode::INTERNAL_SERVER_ERROR, e.to_string())
@@ -16,6 +18,7 @@ pub async fn get_all_logs() -> Result<Json<Vec<AccessLog>>, (StatusCode, String)
 }
 
 pub async fn get_all_persons() -> Result<Json<Vec<PersonaTable>>, (StatusCode, String)> {
+    info!("get_all_persons() called.");
     fetch_persons().await.map(Json).map_err(|e| {
         error!("Database error: {:?}", e);
         (StatusCode::INTERNAL_SERVER_ERROR, e.to_string())
@@ -23,6 +26,7 @@ pub async fn get_all_persons() -> Result<Json<Vec<PersonaTable>>, (StatusCode, S
 }
 
 pub async fn get_fractal_handler(Query(params): Query<FractalParams>) -> Json<Vec<FractalPoint>> {
+
     let max_iterations = params.max_iterations.unwrap_or(500);
 
     let default_bounds = if params.kind == 1 {
@@ -35,6 +39,11 @@ pub async fn get_fractal_handler(Query(params): Query<FractalParams>) -> Json<Ve
         (Some(x_min), Some(x_max), Some(y_min), Some(y_max)) => Bounds { x_min, x_max, y_min, y_max },
         _ => default_bounds,
     };
+    
+    info!(
+        "get_fractal_handler() called, kind: {:?}, bounds: {:?}, max_iterations: {}",
+        params.kind, bounds, max_iterations
+    );
 
     let points = match params.kind {
         1 => generate_mandelbrot(bounds, max_iterations),
@@ -47,5 +56,8 @@ pub async fn get_fractal_handler(Query(params): Query<FractalParams>) -> Json<Ve
 }
 
 pub async fn generate_random_vertex() -> String {
+
+    info!("generate_random_vertex() called.");
+    
     run_random_dijkstra()
 }
